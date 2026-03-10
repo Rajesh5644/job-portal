62d299cf12f7025788ee77d2bbe55af6687448c2
Rajesh medari <medarirajesh99@gmail.com>
2026-03-10 14:07:48 +0530

    updates

--- server.js
+++ server.js
@@ -28,8 +28,6 @@ app.use(session({
 // DATABASE
 const { Pool } = require("pg");
 
-const { Pool } = require("pg");
-
 const db = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false }
